0b7ee022386c0ff620079c7c1302cfd9b9707d41
Richard C <RLCritchlow.iii@gmail.com>
2020-07-31 09:30:45 -0400

    Changed test.c/h to LibraryTesting.c/h

---
+++ LibraryTesting.h
@@ -0,0 +1,13 @@
+/*
+ * test.h
+ *
+ *  Created on: Jul 28, 2020
+ *      Author: Corbin Bremmeyr
+ */
+
+#ifndef LIBRARYTESTING_H_
+#define LIBRARYTESTING_H_
+
+void test_run(void);
+
+#endif /* LIBRARYTESTING_H_ */
